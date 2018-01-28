adc12202c9c77e1566307f525598ffd82b9ef377
Srđan Rašić <srdan.rasic@gmail.com>
2018-01-28 18:44:32 +0100

    Initial Commit

---
+++ Layoutless/Layoutless.h
@@ -0,0 +1,19 @@
+//
+//  Layoutless.h
+//  Layoutless
+//
+//  Created by Srdan Rasic on 28/01/2018.
+//  Copyright © 2018 Absurd Abstractions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Layoutless.
+FOUNDATION_EXPORT double LayoutlessVersionNumber;
+
+//! Project version string for Layoutless.
+FOUNDATION_EXPORT const unsigned char LayoutlessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Layoutless/PublicHeader.h>
+
+
